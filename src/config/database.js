a2a17431ecdb82bd84c3f5389f0ee16554dd6c5e
Çağlar <kcaglarozen@outlook.com>
2023-12-05 13:40:37 +0300

    Proces.env ile yazıldı

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,23 @@
-
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+// .env dosyasını yükle
+dotenv.config();
+
+// MongoDB bağlantı dizesini çevresel değişkenden al
 const uri = process.env.MONGODB_URI;
 
+// MongoDB bağlantısını gerçekleştir
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const connection = mongoose.connection;
+
+// Bağlantı açıldığında bir kere çalışacak olan fonksiyon
 connection.once('open', () => {
     console.log('MongoDB veritabanına başarıyla bağlandı');
+});
+
+// Hata durumunda konsola yazdır
+connection.on('error', (err) => {
+    console.error('MongoDB bağlantı hatası:', err);
 });
