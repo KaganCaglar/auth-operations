f5e29939d9022ea7e7008928f5c42175ff50fb74
Çağlar <kcaglarozen@outlook.com>
2023-12-14 11:52:07 +0300

    user login error bug fixed

--- src/config/passport_local.js
+++ src/config/passport_local.js
@@ -7,51 +7,41 @@ module.exports = function (passport) {
         usernameField: 'email',
         passwordField: 'sifre'
     };
-    passport.use(new LocalStrategy(options, async (email, sifre, done) => {
-        
 
+    passport.use(new LocalStrategy(options, async (email, sifre, done) => {
         try {
             const _bulunanUser = await User.findOne({ email: email });
-            isUserExist(_bulunanUser)
-            
+
+            // isUserExist fonksiyonunu çağırırken done fonksiyonunu parametre olarak geçirin
+            isUserExist(_bulunanUser, done);
 
             const sifreKontrol = await bcrypt.compare(sifre, _bulunanUser.sifre);
             if (!sifreKontrol) {
                 return done(null, false, { message: 'Şifre hatalı' });
             } else {
-
                 if (_bulunanUser && _bulunanUser.emailAktif === false) {
                     return done(null, false, { message: 'Lütfen emailiniz onaylayın' });
-                }else 
+                } else {
                     return done(null, _bulunanUser);
+                }
             }
-
-           
-
-           
-
-            
         } catch (err) {
             return done(err);
         }
-
-
-
     }));
 
     passport.serializeUser(function (user, done) {
-       
         done(null, user.id);
-      });
-      
-      passport.deserializeUser(function (id, done) {
+    });
+
+    passport.deserializeUser(function (id, done) {
         User.findById(id, function (err, user) {
             if (err || !user) {
                 return done(err, null);
             }
-    
+
             const { id, email, ad, soyad, sifre, createdAt, avatar } = user;
-    
+
             const yeniUser = {
                 id,
                 email,
@@ -61,18 +51,18 @@ module.exports = function (passport) {
                 olusturulmaTarihi: createdAt,
                 avatar
             };
-    
+
             done(null, yeniUser);
         });
     });
-    
 
+    // isUserExist fonksiyonunu, done parametresini kullanarak düzenleyin
+    async function isUserExist(user, done) {
+        if (!user) {
+            return done(null, false, { message: 'User bulunamadı' });
+        }
 
-
-
-function  isUserExist(user) {
-    if (!user) {
-        return done(null, false, { message: 'User bulunamadı' });
+        // Eğer kullanıcı bulunduysa, işlemlerinizi buraya ekleyebilirsiniz.
+        // Örneğin, kullanıcının varlığı durumunda başka bir şeyler yapabilirsiniz.
     }
-}
 }
